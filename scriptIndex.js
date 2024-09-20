ea3edfe4fef174c81c0acb50ae4a225b68a78600
Artem Litvintsev <61841951+fatarick@users.noreply.github.com>
2024-09-20 21:59:00 +0300

    Add files via upload

---
+++ scriptIndex.js
@@ -0,0 +1,19 @@
+
+function accept(){
+    location.reload();
+    console.log("Accepted");
+}
+
+function decline(){
+    location.reload();
+    console.log("Declined");
+}
+
+function skip(){
+    location.reload();
+    console.log("Skipped");
+}
+
+function redirectToAbout(){
+    location.href = "about.html";
+}
