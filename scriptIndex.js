e0fd8695f7067ef571be3d87df3e2e1f0b143729
Artem Litvintsev <61841951+fatarick@users.noreply.github.com>
2024-10-20 08:53:48 +0300

    Update scriptIndex.js

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,17 +1,33 @@
+var accepted = 0;
+var skipped = 0;
+var rejected = 0;
+
+function reload(){
+    var images = document.getElementsByTagName("catimg");
+    for (var i = 0; i < images.length; i++) {
+        images[i].src = "https://cataas.com/cat";
+    }
+}
 
 function accept(){
-    location.reload();
-    console.log("Accepted");
+    accepted++;
+    console.log("Accepted: " + accepted);
+    reload();
+    document.getElementById("accepted").innerHTML = "Accepted: " + accepted;
 }
 
 function skip(){
-    location.reload();
-    console.log("Skipped!");
+    skipped++;
+    console.log("Skipped: " + skipped);
+    reload();
+    document.getElementById("skipped").innerHTML = "Skipped: " + skipped;
 }
 
 function reject(){
-    location.reload();
-    console.log("Rejected");
+    rejected++;
+    console.log("Rejected: " + rejected);
+    reload();
+    document.getElementById("rejected").innerHTML = "Rejected: " + rejected;
 }
 
 function redirectToAbout(){
